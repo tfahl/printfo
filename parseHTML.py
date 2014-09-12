b1153a149693bec304c98488e89c9b57f6ba882d
tfahl <pizzamanzootoo@gmail.com>
2014-09-12 21:14:36 +0200

    Cleaned parseHTML

--- parseHTML.py
+++ parseHTML.py
@@ -1,19 +1,19 @@
 from HTMLParser import HTMLParser
 
 #Creates subclass to override handler methods
-class my_hp(HTMLParser):
+class html_parser(HTMLParser):
 	def handle_starttag(self, tag, attrs):
 		print "Encountered a start tag"
 
 #Creates a parser
-parser = my_hp()
+parser = html_parser()
 
 #opens an html file, stores text in varaible data
 #Replace test/test.html with html file to parse
-with open('test/test.html', 'r') as myfile:
-	data = myfile.read().replace('\n','')
+with open('test/test.html', 'r') as html_input:
+	html_text = html_input.read().replace('\n','')
 
 #Runs parser on text contents of the html file
-parser.feed(data)
+parser.feed(html_text)
 
 
