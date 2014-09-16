9b8d01ba29448744c4f55e23b068938c6d606737
tfahl <pizzamanzootoo@gmail.com>
2014-09-16 19:33:20 +0200

    updated

--- parseHTML.py
+++ parseHTML.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
 
+file = open('test/data.csv', 'w+')
+
+def write_file(data):
+	file.write(data + ",")
+
+
+
 #uses beautiful soup to parse html file
 #finds the correct span tag
 #Gets the percentage of ink left in the printer
@@ -7,6 +14,6 @@ soup = BeautifulSoup(open("test/test.html"))
 res =  soup.find('span',{'class':'hpConsumableBlockHeaderText'}).text
 num = res[24] + res[25]
 
-file = open('test/data.csv', 'w+') 
-file.write(num + ",")
+write_file(num)
 
+file.close()
