44226dabb65cb06522c128539660e407e53ca602
tfahl <pizzamanzootoo@gmail.com>
2014-09-16 19:15:28 +0200

    Write information from test.html to csv file

--- parseHTML.py
+++ parseHTML.py
@@ -5,5 +5,8 @@ from bs4 import BeautifulSoup
 #Gets the percentage of ink left in the printer
 soup = BeautifulSoup(open("test/test.html"))
 res =  soup.find('span',{'class':'hpConsumableBlockHeaderText'}).text
-num = res[24] + res[25] 
+num = res[24] + res[25]
+
+file = open('test/data.csv', 'w+') 
+file.write(num + ",")
 
