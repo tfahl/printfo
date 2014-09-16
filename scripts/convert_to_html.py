59460d5f691b61c747e233763d880dc0e6fd963a
tfahl <pizzamanzootoo@gmail.com>
2014-09-16 22:39:16 +0200

    ran html converter

--- scripts/convert_to_html.py
+++ scripts/convert_to_html.py
@@ -1,4 +1,8 @@
-with open('this.LCDispatcher','r') as printer_file:
+dir = '../printer_sources/'
+dis_dir = 'dispatch/'
+html_dir = 'html/'
+
+with open(dir + dis_dir + 'this.LCDispatcher','r') as printer_file:
     html_file = printer_file.read().replace('\n','')
-file  = open('test.html', 'w+')
+file  = open(dir + html_dir + 'dirtest.html', 'w+')
 file.write(html_file)
