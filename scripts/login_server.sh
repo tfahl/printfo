f29750f166efd0e0469a227215b43f9627cd42c6
tfahl <pizzamanzootoo@gmail.com>
2014-09-17 00:01:58 +0200

    Added a login script for getting printer sources from osu engr server

---
+++ scripts/login_server.sh
@@ -0,0 +1,5 @@
+#runs python script on osu engr server to get updated printer pages
+ssh fahlmant@access.engr.oregonstate.edu python runner.py
+#need to run a script that copies printer pages from osu engr server to local server
+
+
